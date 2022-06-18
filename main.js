144e6166f1e693a7270cc16a46c9723f2ab695a6
Alexandr <alexgriko29@gmail.com>
2022-06-18 15:52:30 +0300

    task lvl 1

--- main.js
+++ main.js
@@ -6,17 +6,40 @@ const left=document.querySelector('.btnleft');
 const down=document.querySelector('.btndown');
 let squere=document.querySelector('.squere');
 
-let steparr=[];
 let step=0;
-const maxw=1000;
-const maxh=800;
+const maxw=1100;
+const maxh=650;
 
 right.addEventListener('click', ()=> {
   if (step<=maxw) {
     step+=50;
-  squere.style.marginLeft=step+'px';
-  console.log('right');
+  squere.style.left=step+'px';
+  return step;
   } else 
   return;
 })
 
+left.addEventListener('click', ()=> {
+  if (step>=50) {
+    step-=50;
+  squere.style.left=step+'px';
+  } else 
+  return;
+})
+
+down.addEventListener('click', ()=> {
+  if (step<=maxh) {
+    step+=50;
+  squere.style.top=step+'px';
+  return step;
+  } else 
+  return;
+})
+
+up.addEventListener('click', ()=> {
+  if (step>=50) {
+    step-=50;
+  squere.style.top=step+'px';
+  } else 
+  return;
+})
